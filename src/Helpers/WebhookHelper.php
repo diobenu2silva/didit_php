94cffd7c3af2259f59250d0c6ef2aa86ec9f01fc
diobenu08koga <silvia.ittech1129@gmail.com>
2025-03-25 18:24:35 +0000

    Fix styling

--- src/Helpers/WebhookHelper.php
+++ src/Helpers/WebhookHelper.php
@@ -7,19 +7,18 @@ class WebhookHelper
     /**
      * Verify an incoming Didit webhook, in a safe way, guarding against timing attacks
      *
-     * @param string $requestBody The raw (text) body of the incoming webhook request
-     * @param string $signatureHeader The `X-Signature` header of the incoming webhook request
-     * @param string $timestampHeader The `X-Timestamp` header of the incoming webhook request
-     * @param string $webhookSecretKey The webhook secret key copied from the Didit dashboard
-     *
+     * @param  string  $requestBody  The raw (text) body of the incoming webhook request
+     * @param  string  $signatureHeader  The `X-Signature` header of the incoming webhook request
+     * @param  string  $timestampHeader  The `X-Timestamp` header of the incoming webhook request
+     * @param  string  $webhookSecretKey  The webhook secret key copied from the Didit dashboard
      * @return bool `true` if webhook is valid/authentic, `false` otherwise
      *
      * @link https://docs.didit.me/identity-verification/webhooks Webhooks
      */
     public static function verifyWebhookSignature(string $requestBody, string $signatureHeader,
-                                                  string $timestampHeader, string $webhookSecretKey): bool
+        string $timestampHeader, string $webhookSecretKey): bool
     {
-        if (abs(time() - (int)$timestampHeader) > 300) {
+        if (abs(time() - (int) $timestampHeader) > 300) {
             return false;
         }
 
