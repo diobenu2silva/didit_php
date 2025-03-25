462ec05446b2005b37e564111e20fc1cedf28068
diobenu08koga <silvia.ittech1129@gmail.com>
2025-03-25 14:23:49 -0400

    Added convenience method to verify `Session` webhooks  d69260

--- src/Helpers/WebhookHelper.php
+++ src/Helpers/WebhookHelper.php
@@ -11,40 +11,15 @@ class WebhookHelper
      * @param string $signatureHeader The `X-Signature` header of the incoming webhook request
      * @param string $timestampHeader The `X-Timestamp` header of the incoming webhook request
      * @param string $webhookSecretKey The webhook secret key copied from the Didit dashboard
-     * @return bool
+     *
+     * @return bool `true` if webhook is valid/authentic, `false` otherwise
+     *
      * @link https://docs.didit.me/identity-verification/webhooks Webhooks
      */
     public static function verifyWebhookSignature(string $requestBody, string $signatureHeader,
                                                   string $timestampHeader, string $webhookSecretKey): bool
     {
         if (abs(time() - (int)$timestampHeader) > 300) {
-            return false;
-        }
-
-        return hash_equals($signatureHeader, hash_hmac('sha256', $requestBody, $webhookSecretKey));
-    }
-}
-
-<?php
-
-namespace AlexStewartJa\Didit\Helpers;
-
-class WebhookHelper
-{
-    /**
-     * Verify an incoming Didit webhook, in a safe way, guarding against timing attacks
-     *
-     * @param  string  $requestBody  The raw (text) body of the incoming webhook request
-     * @param  string  $signatureHeader  The `X-Signature` header of the incoming webhook request
-     * @param  string  $timestampHeader  The `X-Timestamp` header of the incoming webhook request
-     * @param  string  $webhookSecretKey  The webhook secret key copied from the Didit dashboard
-     *
-     * @link https://docs.didit.me/identity-verification/webhooks Webhooks
-     */
-    public static function verifyWebhookSignature(string $requestBody, string $signatureHeader,
-        string $timestampHeader, string $webhookSecretKey): bool
-    {
-        if (abs(time() - (int) $timestampHeader) > 300) {
             return false;
         }
 
